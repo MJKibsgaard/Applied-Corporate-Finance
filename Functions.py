b239df4c008f26bbc718d27546d8c1b0882563c5
MJKibsgaard <99269954+MJKibsgaard@users.noreply.github.com>
2023-09-12 22:12:14 +0200

    Test

---
+++ Functions.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+
+
+def calculate_daily_returns(tickers, start, end):
+    # Opret en tom DataFrame til at gemme daglige afkast
+    daily_returns = pd.DataFrame()
+
+    # Hent data for hver aktie og beregn daglige afkast
+    for ticker in tickers:
+        data = yf.download(ticker, start=start, end=end)
+        adj_close = data['Adj Close']
+        daily_returns[ticker] = adj_close.pct_change()
+
+    return daily_returns
---
+++ Project.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import pandas as pd
+from Functions import calculate_daily_returns
+
+# Liste over danske C25-virksomheder og deres tickers
+c25_companies = [
+    'CHR.CO', 'DANSKE.CO', 'DSV.CO', 'FLS.CO', 'GEN.CO', 'LUN.CO', 'MAERSK-A.CO', 'MAERSK-B.CO', 
+    'NOVO-B.CO', 'NZYM-B.CO', 'ORSTED.CO', 'PNDORA.CO', 'TRYG.CO', 'VWS.CO'
+]
+
+# Start- og slutdato for din dataindsamling
+start_date = '2023-01-01'
+end_date = '2023-09-01'
+
+# Hent data for hver C25-virksomhed
+c25_data = yf.download(c25_companies, start=start_date, end=end_date)
+
+# Vis de første rækker af data for alle virksomheder
+print(c25_data.head())
+
+# Kald funktionen med C25-virksomhederne
+c25_daily_returns = calculate_daily_returns(c25_companies, start_date, end_date)
+
+# Print nogle data fra funktionen
+print(c25_daily_returns.head())
+
+# Gem daglige afkast i en Excel-fil
+c25_daily_returns.to_excel('daily_returns.xlsx', index=True)
+
+
+
+
+
+
