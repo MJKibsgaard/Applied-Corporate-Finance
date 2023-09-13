e654d0a10fabb349f7283403f8dbaa022ddbf344
MJKibsgaard <99269954+MJKibsgaard@users.noreply.github.com>
2023-09-13 06:41:40 +0200

    test

--- DataCollecting.py
+++ DataCollecting.py
@@ -14,37 +14,14 @@ end_date = '2023-09-01'
 # Hent data for hver C25-virksomhed
 c25_data = yf.download(c25_companies, start=start_date, end=end_date)
 
+# Vis de første rækker af data for alle virksomheder
+print(c25_data.head())
+
 # Kald funktionen med C25-virksomhederne
 c25_daily_returns = calculate_daily_returns(c25_companies, start_date, end_date)
-
-# Opret en kolonne "Ticker" baseret på indeksnavnet
-c25_daily_returns['Ticker'] = c25_daily_returns.index
-
-# Hent andre data
-pe_ratios = {}
-market_caps = {}
-
-for ticker in c25_companies:
-    info = yf.Ticker(ticker).info
-    pe_ratios[ticker] = info.get('trailingPE')
-    market_caps[ticker] = info.get('marketCap')
-
-# Opret DataFrames for P/E-forhold og Market Capitalization
-pe_df = pd.DataFrame(list(pe_ratios.items()), columns=['Ticker', 'PE Ratio'])
-market_cap_df = pd.DataFrame(list(market_caps.items()), columns=['Ticker', 'Market Cap'])
-
-# Sæt "Ticker" som indeks i P/E-forhold og Market Capitalization DataFrames
-pe_df.set_index('Ticker', inplace=True)
-market_cap_df.set_index('Ticker', inplace=True)
-
-# Flet data om P/E-forhold og Market Capitalization med c25_daily_returns
-c25_daily_returns = pd.concat([c25_daily_returns, pe_df, market_cap_df], axis=1, join='inner')
 
 # Print nogle data fra funktionen
 print(c25_daily_returns.head())
 
-# Fjern den midlertidige "Ticker" kolonne
-c25_daily_returns.drop(columns=['Ticker'], inplace=True)
-
-# Gem daglige afkast inklusive ekstra data i en Excel-fil
-c25_daily_returns.to_excel('daily_returns_with_data.xlsx', index=True)
+# Gem daglige afkast i en Excel-fil
+c25_daily_returns.to_excel('daily_returns.xlsx', index=True)
